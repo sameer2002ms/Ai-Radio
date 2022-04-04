98d3a600815b1f46c1dc981fa7ff4a34641bca18
sameer <sameer2002.ms@gmail.com>
2022-04-04 12:21:37 +0530

    push

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:ai_radio/model/radio.dart';
 import 'package:ai_radio/utils/ai_utils.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -10,20 +11,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+     late List<MyRadio> radios;
 
-
-  late List <MyRadio> radios;
   @override
   void initState() {
     super.initState();
     fetchRadios();
   }
-  fetchRadios()  async {
+
+  fetchRadios() async {
     final radioJson = await rootBundle.loadString("assets/radio.json");
     radios = MyRadioList.fromJson(radioJson).radios;
     print(radios);
+    setState(() {});
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -50,16 +51,56 @@ class _HomePageState extends State<HomePage> {
           //shimmer is used for effect
           AppBar(
             title: "AI Radio".text.xl2.bold.white.make().shimmer(
-              primaryColor: Vx.purple300, secondaryColor: Colors.white
-            ),
+                primaryColor: Vx.purple300, secondaryColor: Colors.white),
             backgroundColor: Colors.transparent,
             elevation: 0.0,
             centerTitle: true,
-          ).h(80).p8()
+          ).h(80).p8(),
+          //it will show the item count and show the radio output
+          VxSwiper.builder(
+            itemCount: radios.length,
+            aspectRatio: 1.0,
+            enlargeCenterPage: true,
+            itemBuilder: (context, index) {
+              final rad = radios[index];
+              return VxBox(child: ZStack([
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: VStack([
+                    rad.name.text.xl3.white.bold.make(),
+                    5.heightBox,
+                    rad.tagline.text.sm.semiBold.make(),
+                  ],
+                  crossAlignment: CrossAxisAlignment.center,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: [Icon(
+                    CupertinoIcons.play_circle,
+                    color: Colors.white,
+                  ),
+                    10.heightBox,
+                    "Double tap to play".text.gray300.make(),
+                  ].vStack())
+
+              ]))
+                  .bgImage(
+                    DecorationImage(
+                        image: NetworkImage(rad.image),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.3), BlendMode.darken)),
+                  )
+              .border(color: Colors.black, width: 5.0)
+                  .withRounded(value: 60.0)
+                  .make()
+              .p16().centered();
+            },
+          )
         ],
+        fit: StackFit.expand,
       ),
-
-
     );
   }
 }
