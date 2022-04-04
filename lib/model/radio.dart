7e8e6fde3d76b90daab78f6bbc09b9664e014873
sameer <sameer2002.ms@gmail.com>
2022-04-04 11:34:42 +0530

    push

--- lib/model/radio.dart
+++ lib/model/radio.dart
@@ -2,52 +2,52 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 
-class MyRadioList{
+class MyRadioList {
   final List<MyRadio> radios;
-  MyRadioList({required this.radios});
+  MyRadioList({
+    required this.radios,
+  });
 
   MyRadioList copyWith({
-  List<MyRadio>? radios,
-}) {
-    return MyRadioList(radios: radios ?? this.radios,);
-  }
-
-  Map<String , dynamic> toMap(){
-    return {
-      'radios' : radios?.map((x) => x?.toMap()) ?.toList(),
-    };
-  }
-
-  factory MyRadioList.fromMap(Map<String, dynamic> map){
-    if(map == null) return null;
-
+    List<MyRadio>? radios,
+  }) {
     return MyRadioList(
-      radios: List<MyRadio>.from(map['radio'] ?.map((e) => MyRadio.fromMap(e))),
+      radios: radios ?? this.radios,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'radios': radios.map((x) => x.toMap()).toList()});
+
+    return result;
+  }
+
+  factory MyRadioList.fromMap(Map<String, dynamic> map) {
+    return MyRadioList(
+      radios: List<MyRadio>.from(map['radios']?.map((x) => MyRadio.fromMap(x))),
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory MyRadioList.fromJson(String source) =>
-      MyRadioList.fromMap(json.decode(source));
+  factory MyRadioList.fromJson(String source) => MyRadioList.fromMap(json.decode(source));
 
   @override
   String toString() => 'MyRadioList(radios: $radios)';
 
   @override
-  bool operator ==(Object o) {
-    if (identical(this, o)) return true;
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
 
-    return o is MyRadioList && listEquals(o.radios, radios);
+    return other is MyRadioList &&
+        listEquals(other.radios, radios);
   }
 
   @override
   int get hashCode => radios.hashCode;
 }
-
-
-
-
 
 class MyRadio {
   final int id;
@@ -61,23 +61,33 @@ class MyRadio {
   final String icon;
   final String image;
   final String lang;
-
-  MyRadio({required this.id, required this.order, required this.name, required this.tagline, required this.color, required this.desc,
-      required this.url, required this.category, required this.icon, required this.image, required this.lang});
+  MyRadio({
+    required this.id,
+    required this.order,
+    required this.name,
+    required this.tagline,
+    required this.color,
+    required this.desc,
+    required this.url,
+    required this.category,
+    required this.icon,
+    required this.image,
+    required this.lang,
+  });
 
   MyRadio copyWith({
-  required int id,
-  required int order,
-  required String name,
-  required String tagline,
-  required String color,
-  required String desc,
-  required String url,
-  required String category,
-  required String icon,
-  required String image,
-  required String lang,
-  }){
+    int? id,
+    int? order,
+    String? name,
+    String? tagline,
+    String? color,
+    String? desc,
+    String? url,
+    String? category,
+    String? icon,
+    String? image,
+    String? lang,
+  }) {
     return MyRadio(
       id: id ?? this.id,
       order: order ?? this.order,
@@ -92,37 +102,38 @@ class MyRadio {
       lang: lang ?? this.lang,
     );
   }
+
   Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'order': order,
-      'name': name,
-      'tagline': tagline,
-      'color': color,
-      'desc': desc,
-      'url': url,
-      'category': category,
-      'icon': icon,
-      'image': image,
-      'lang': lang,
-    };
-}
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'order': order});
+    result.addAll({'name': name});
+    result.addAll({'tagline': tagline});
+    result.addAll({'color': color});
+    result.addAll({'desc': desc});
+    result.addAll({'url': url});
+    result.addAll({'category': category});
+    result.addAll({'icon': icon});
+    result.addAll({'image': image});
+    result.addAll({'lang': lang});
+
+    return result;
+  }
 
   factory MyRadio.fromMap(Map<String, dynamic> map) {
-    if (map == null) return null;
-
     return MyRadio(
-      id: map['id'],
-      order: map['order'],
-      name: map['name'],
-      tagline: map['tagline'],
-      color: map['color'],
-      desc: map['desc'],
-      url: map['url'],
-      category: map['category'],
-      icon: map['icon'],
-      image: map['image'],
-      lang: map['lang'],
+      id: map['id']?.toInt() ?? 0,
+      order: map['order']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      tagline: map['tagline'] ?? '',
+      color: map['color'] ?? '',
+      desc: map['desc'] ?? '',
+      url: map['url'] ?? '',
+      category: map['category'] ?? '',
+      icon: map['icon'] ?? '',
+      image: map['image'] ?? '',
+      lang: map['lang'] ?? '',
     );
   }
 
@@ -137,21 +148,21 @@ class MyRadio {
   }
 
   @override
-  bool operator ==(Object o) {
-    if (identical(this, o)) return true;
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
 
-    return o is MyRadio &&
-        o.id == id &&
-        o.order == order &&
-        o.name == name &&
-        o.tagline == tagline &&
-        o.color == color &&
-        o.desc == desc &&
-        o.url == url &&
-        o.category == category &&
-        o.icon == icon &&
-        o.image == image &&
-        o.lang == lang;
+    return other is MyRadio &&
+        other.id == id &&
+        other.order == order &&
+        other.name == name &&
+        other.tagline == tagline &&
+        other.color == color &&
+        other.desc == desc &&
+        other.url == url &&
+        other.category == category &&
+        other.icon == icon &&
+        other.image == image &&
+        other.lang == lang;
   }
 
   @override
